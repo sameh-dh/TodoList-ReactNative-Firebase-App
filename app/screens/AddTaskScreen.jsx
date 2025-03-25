b8ca35c2308b05698edb9374ea3158898bc59854
sameh-dh <samehdh44@gmail.com>
2025-03-25 21:50:49 +0100

    working on the add task file

--- app/screens/AddTaskScreen.jsx
+++ app/screens/AddTaskScreen.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const AddTaskScreen = () => {
+import React , {useState}from 'react'
+import { View, TextInput, Button , Text} from 'react-native'
+// import {useTasks} from '../hooks/useTasks'
+
+const AddTaskScreen = ({navigation}) => {
+const [task, setTask] =useState('');
+// const {addTask} = useTasks();
+
+const handleAddTask = () => {
+  // addTask(task);
+setTask('')
+navigation.goBack();}
   return (
     <View>
-      <Text>AddTaskScreen</Text>
+      <TextInput placeholder='EnterTask' value={task} onChangeText={setTask} style={{"backgroundColor":"pink", "width":"50px"}}></TextInput>
+      <Button title="Add Task" onPress={handleAddTask} />
     </View>
   )
 }
--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,24 +1,21 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
-import { TaskContext  } from '../context/TaskContext';
-import TaskItem from '../components/TaskItem';
+import React, { useContext } from "react";
+import { View, Text, Button } from "react-native";
+import { TaskContext } from "../context/TaskContext";
+import TaskItem from "../components/TaskItem";
 
 const HomeScreen = ({ navigation }) => {
+  const { tasks } = useContext(TaskContext);
 
-  const {tasks} = useContext(TaskContext);
-  return(
+  return (
     <View>
-        <Text>To-Do List</Text>
-        {tasks.map((task, index) =>{
-          return <TaskItem key={index} task={task} />
-        })}
-        
-        <Button 
-        title="Add Task"
-         OnPress ={()=> navigation.navigate('AddTask')}/>
+      <Text>To-Do List</Text>
+      {tasks.map((task, index) => {
+        return <TaskItem key={index} task={task} />;
+      })}
+
+      <Button title="Add Task" onPress={() => navigation.navigate("AddTask")} />
     </View>
   );
 };
 
 export default HomeScreen;
-
