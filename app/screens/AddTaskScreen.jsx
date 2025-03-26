e5bd33e46abcf2d8478a9ee86dba11250b4bc32e
sameh-dh <samehdh44@gmail.com>
2025-03-26 22:37:01 +0100

    add usetasks and work on it , file will be improved...

--- app/screens/AddTaskScreen.jsx
+++ app/screens/AddTaskScreen.jsx
@@ -1,22 +1,27 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, Text } from "react-native";
+import { useTasks } from "../hooks/useTasks";
 
-import React , {useState}from 'react'
-import { View, TextInput, Button , Text} from 'react-native'
-// import {useTasks} from '../hooks/useTasks'
+const AddTaskScreen = ({ navigation }) => {
+  const [task, setTask] = useState("");
+  const { addTask } = useTasks();
 
-const AddTaskScreen = ({navigation}) => {
-const [task, setTask] =useState('');
-// const {addTask} = useTasks();
-
-const handleAddTask = () => {
-  // addTask(task);
-setTask('')
-navigation.goBack();}
+  const handleAddTask = () => {
+    addTask(task);
+    setTask("");
+    navigation.goBack();
+  };
   return (
     <View>
-      <TextInput placeholder='EnterTask' value={task} onChangeText={setTask} style={{"backgroundColor":"pink", "width":"50px"}}></TextInput>
+      <TextInput
+        placeholder="EnterTask"
+        value={task}
+        onChangeText={setTask}
+        style={{ backgroundColor: "pink", width: "50px" }}
+      ></TextInput>
       <Button title="Add Task" onPress={handleAddTask} />
     </View>
-  )
-}
+  );
+};
 
-export default AddTaskScreen
+export default AddTaskScreen;
